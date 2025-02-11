a4db96b623f195ebe88d3159b14ee3b01857ddad
devseanvaldez <dev.seanvaldez@gmail.com>
2025-02-11 21:33:25 +0800

    Auto refresh Homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the homepage on refresh
+    if (typeof window !== "undefined") {
+      router.replace("/");
+    }
+  }, []);
+
   return (
-    <div className="dark:bg-black text-black dark:text-white">
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-black text-black dark:text-white">
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   );
 }
